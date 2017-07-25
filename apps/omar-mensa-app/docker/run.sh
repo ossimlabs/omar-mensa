3615e1ff27894c8e080c60816562f3fae2a856b0
Garrett Potts <gpotts@radiantblue.com>
2017-07-25 08:46:31 -0400

    Added nss wrappers

--- apps/omar-mensa-app/docker/run.sh
+++ apps/omar-mensa-app/docker/run.sh
@@ -3,6 +3,16 @@ if [ ! -z $AWS_ACCESS_KEY ] ; then
   export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
   export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
 fi
+
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+sed '/^omar/d' /etc/passwd > /tmp/passwd
+echo omar:x:$USER_ID:$GROUP_ID:Default Application User:$HOME:/sbin/nologin >> /tmp/passwd
+
+export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+
 mkdir -p /s3/$BUCKET
 #echo "BUCKET = ${BUCKET}"
 riofs -o allow_other -c /usr/share/omar/riofs.conf.xml  $BUCKET /s3/$BUCKET
