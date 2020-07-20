3628e791ea76af6779b4cc31b8e6b1191d2156e4
aimee <aimee@localhost.localdomain>
2020-07-20 16:35:00 -0400

    Made working tests (not dynamic yet)

--- cypress/integration/APITesting.js
+++ cypress/integration/APITesting.js
@@ -8,27 +8,112 @@ describe('Automated tests for the omar-mensa methods', () => {
         methods.forEach((method) => {
             innerJson = json.paths[path][method];
             name = innerJson["operationId"];
-            parameters = innerJson["parameters"]
-            request = "?"
-            parameters.forEach((parameter) => {
-                if(parameter["default"])
-                    request = request + parameter["name"] + "=" + parameter["default"] + "&";
-                if(parameter["enum"])
-                    request = request + parameter["name"] + "=" + parameter["enum"][0] + "&";
-            })
-            request = request.substring(0, request.length - 1);
-            it(`Should test 200 code for ${name} default values`, () => {
-                cy.request(method, path + request)
-                    .then((response) => {
-                        expect(response.status).to.eq(200)
+            parameters = innerJson["parameters"];
+            if (parameters[0]["in"] == "body" && parameters[0]["schema"]["default"]) {
+                if (name === "imageDistance") {
+                    it(`Should test 200 code for ${name} default values`, () => {
+                        cy.request({
+                            method: method, url: path, body: {
+                                "filename": "/data/geoint/strips/056599362030_01/056599362030_01_P002_MUL/17APR19054130-M1BS-056599362030_01_P002.NTF",
+                                "entryId": 0,
+                                "pointList": "LINESTRING(14003.649810791016 15219.103088378904, 14774.048736572266 15271.243347167967)"
+                            }
+                        })
+                            .then((response) => {
+                                expect(response.status).to.eq(200)
+                            })
                     })
-            })
-            it(`Should test response header for ${name}`, () => {
-                 cy.request(method, path + request)
-                     .then((response) => {
-                         expect(response).to.have.property("headers")
-                     })
-            })
+                    it(`Should test response header for ${name}`, () => {
+                        cy.request(method, path, {
+                            "filename": "/data/geoint/strips/056599362030_01/056599362030_01_P002_MUL/17APR19054130-M1BS-056599362030_01_P002.NTF",
+                            "entryId": 0,
+                            "pointList": "LINESTRING(14003.649810791016 15219.103088378904, 14774.048736572266 15271.243347167967)"
+                        })
+                            .then((response) => {
+                                expect(response).to.have.property("headers")
+                            })
+                    })
+                } else if (name === "imagePointsToGround") {
+                    it(`Should test 200 code for ${name} default values`, () => {
+                        cy.request({
+                            method: method, url: path, body: {
+                                "filename": "/data/geoint/strips/056599362030_01/056599362030_01_P002_MUL/17APR19054130-M1BS-056599362030_01_P002.NTF",
+                                "entryId": 0,
+                                "pointList": [
+                                    {"x":14003.649810791016,"y":15219.103088378904},
+                                    {"x":14774.048736572266,"y":15271.243347167967}
+                                ],
+                                "pqeIncludePositionError":false,
+                                "pqeProbabilityLevel" : 0.9,
+                                "pqeEllipsePointType" : "none",
+                                "pqeEllipseAngularIncrement": 10
+                            }
+                        })
+                            .then((response) => {
+                                expect(response.status).to.eq(200)
+                            })
+                    })
+                    it(`Should test response header for ${name}`, () => {
+                        cy.request(method, path, {
+                            "filename": "/data/geoint/strips/056599362030_01/056599362030_01_P002_MUL/17APR19054130-M1BS-056599362030_01_P002.NTF",
+                            "entryId": 0,
+                            "pointList": [
+                                {"x":14003.649810791016,"y":15219.103088378904},
+                                {"x":14774.048736572266,"y":15271.243347167967}
+                            ],
+                            "pqeIncludePositionError":false,
+                            "pqeProbabilityLevel" : 0.9,
+                            "pqeEllipsePointType" : "none",
+                            "pqeEllipseAngularIncrement": 10
+                        })
+                            .then((response) => {
+                                expect(response).to.have.property("headers")
+                            })
+                    })
+                } else if (name === "groundToImagePoints") {
+                    it(`Should test 200 code for ${name} default values`, () => {
+                        cy.request({
+                            method: method, url: path, body: {
+                                "filename": "/data/geoint/strips/056599362030_01/056599362030_01_P002_MUL/17APR19054130-M1BS-056599362030_01_P002.NTF",
+                                "entryId": 0,
+                                "pointList": [
+                                    {"lat":14003.649810791016,"lon":15219.103088378904,hgt:0.0},
+                                    {"lat":14774.048736572266,"lon":15271.243347167967}]
+                            }
+                        })
+                            .then((response) => {
+                                expect(response.status).to.eq(200)
+                            })
+                    })
+                    it(`Should test response header for ${name}`, () => {
+                        cy.request(method, path, {
+                            "filename": "/data/geoint/strips/056599362030_01/056599362030_01_P002_MUL/17APR19054130-M1BS-056599362030_01_P002.NTF",
+                            "entryId": 0,
+                            "pointList": [
+                                {"lat":14003.649810791016,"lon":15219.103088378904,hgt:0.0},
+                                {"lat":14774.048736572266,"lon":15271.243347167967}]
+                        })
+                            .then((response) => {
+                                expect(response).to.have.property("headers")
+                            })
+                    })
+                } else {
+                    it(`Should test 200 code for ${name} default values`, () => {
+                        cy.request({
+                            method: method, url: path, body: JSON.parse(parameters[0]["schema"]["default"])
+                        })
+                            .then((response) => {
+                                expect(response.status).to.eq(200)
+                            })
+                    })
+                    it(`Should test response header for ${name}`, () => {
+                        cy.request(method, path, JSON.parse(parameters[0]["schema"]["default"]))
+                            .then((response) => {
+                                expect(response).to.have.property("headers")
+                            })
+                    })
+                }
+            }
         })
     })
 })
